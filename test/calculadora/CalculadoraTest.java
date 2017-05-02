da96c659825b3f4508a5feba02e33ebca4e772f5
Bruno Moraes <bruno_msarmento@hotmail.com>
2017-05-02 13:02:58 -0300

    Trabalho Calculadora JUnit

---
+++ test/calculadora/CalculadoraTest.java
@@ -0,0 +1,88 @@
+package calculadora;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+
+public class CalculadoraTest {
+ 
+    public CalculadoraTest() {
+    }
+ 
+    @BeforeClass
+    public static void setUpClass() throws Exception {
+    }
+ 
+    @AfterClass
+    public static void tearDownClass() throws Exception {
+    }
+ 
+    @Before
+    public void setUp() {
+    }
+ 
+    @After
+    public void tearDown() {
+    }
+ 
+    /**
+     * Test of somar method, of class Calculadora.
+     */
+    @Test
+    public void testSomar() {
+        System.out.println("somar");
+        int n1 = 25;
+        int n2 = 25;
+        Calculadora instance = new Calculadora();
+        double expResult = 50.0;
+        double result = instance.somar(n1, n2);
+        assertEquals(expResult, result, 0);
+    }
+ 
+    /**
+     * Test of subtrair method, of class Calculadora.
+     */
+    @Test
+    public void testSubtrair() {
+        System.out.println("subtrair");
+        int n1 = 23;
+        int n2 = 10;
+        Calculadora instance = new Calculadora();
+        double expResult = 13;
+        double result = instance.subtrair(n1, n2);
+        assertEquals(expResult, result, 0);
+    }
+ 
+    /**
+     * Test of multiplicar method, of class Calculadora.
+     */
+    @Test /** falhar de proposito **/
+    public void testMultiplicar() {
+        System.out.println("multiplicar");
+        int n1 = 10;
+        int n2 = 3;
+        Calculadora instance = new Calculadora();
+        double expResult = 23;
+        double result = instance.multiplicar(n1, n2);
+        assertEquals(expResult, result, 0);
+    }
+ 
+    /**
+     * Test of dividir method, of class Calculadora.
+     */
+    @Test
+    public void testDividir() {
+        System.out.println("dividir");
+        int n1 = 10;
+        int n2 = 2;
+        Calculadora instance = new Calculadora();
+        double expResult = 5.0;
+        double result = instance.dividir(n1, n2);
+        assertEquals(expResult, result, 0);
+    }
+ 
+}
